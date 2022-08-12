ff4435cff6f82969f002967a97ee1947959b92c6
Olivier Benz <olivier.benz@b-data.ch>
2022-08-12 19:57:21 +0200

    base: Update jupyter_server_config.py
    - base: Update jupyter_notebook_config.py

--- base/scripts/etc/jupyter/jupyter_notebook_config.py
+++ base/scripts/etc/jupyter/jupyter_notebook_config.py
@@ -11,7 +11,7 @@ from jupyter_core.paths import jupyter_data_dir
 c = get_config()
 c.NotebookApp.ip = "0.0.0.0"
 c.NotebookApp.port = 8888
-c.NotebookApp.open_browser = False
+#c.NotebookApp.open_browser = False
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
--- base/scripts/etc/jupyter/jupyter_server_config.py
+++ base/scripts/etc/jupyter/jupyter_server_config.py
@@ -11,7 +11,7 @@ from jupyter_core.paths import jupyter_data_dir
 c = get_config()
 c.ServerApp.ip = "0.0.0.0"
 c.ServerApp.port = 8888
-c.ServerApp.open_browser = False
+#c.ServerApp.open_browser = False
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
