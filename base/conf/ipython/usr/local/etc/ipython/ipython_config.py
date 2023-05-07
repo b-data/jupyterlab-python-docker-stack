47f5b7595628613a6c27ac324510496da0895a5e
Olivier Benz <olivier.benz@b-data.ch>
2023-05-07 17:49:07 +0200

    IPython: Includes user's private bin in PATH
    - Remove superfluous ipython_kernel_config.py

--- base/conf/ipython/usr/local/etc/ipython/ipython_config.py
+++ base/conf/ipython/usr/local/etc/ipython/ipython_config.py
@@ -1,4 +1,26 @@
 # Copyright (c) 2022 b-data GmbH.
 # Distributed under the terms of the MIT License.
 
-c.InlineBackend.figure_formats = {'svg', 'pdf'}
+c.InlineBackend.figure_formats = {"svg", "pdf"}
+
+import os
+
+if (
+    os.path.exists(os.path.join(os.environ["HOME"], "bin"))
+    and os.getenv("SHLVL") == "0"
+):
+    os.environ["PATH"] = (
+        os.path.join(os.environ["HOME"], "bin")
+        + os.pathsep
+        + os.getenv("PATH", "")
+    )
+
+if (
+    os.path.exists(os.path.join(os.environ["HOME"], ".local/bin"))
+    and os.getenv("SHLVL") == "0"
+):
+    os.environ["PATH"] = (
+        os.path.join(os.environ["HOME"], ".local/bin")
+        + os.pathsep
+        + os.getenv("PATH", "")
+    )
