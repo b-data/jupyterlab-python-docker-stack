1bba280f936a3735202e551ac4eb30b83a057ef8
Olivier Benz <olivier.benz@b-data.ch>
2023-07-02 11:39:36 +0200

    Update script limits.sh

--- base/scripts/usr/local/bin/before-notebook.d/limits.sh
+++ base/scripts/usr/local/bin/before-notebook.d/limits.sh
@@ -4,6 +4,11 @@
 
 set -e
 
+DIVISOR=1024
+FACTOR=$(echo 1 ${SWAP_ENABLE:+${SWAP_FACTOR:-1}} |
+  awk '{ printf "%.1f", $1 + $2 }')
+
 if [ ! -z "$MEM_LIMIT" ]; then
-  ulimit -Sv $(($MEM_LIMIT/1024))
+  ulimit -Sv $(echo $MEM_LIMIT $DIVISOR $FACTOR |
+    awk '{ printf "%.0f", $1 / $2 * $3 }')
 fi
