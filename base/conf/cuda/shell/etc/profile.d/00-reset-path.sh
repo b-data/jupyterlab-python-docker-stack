19f2b0e19b7a9c7f8e69b9367934c8ca2ca0a973
Olivier Benz <olivier.benz@b-data.ch>
2024-08-01 11:51:36 +0200

    Unify default PATH settings
    - Same PATH as in the Dockerfile

--- base/conf/cuda/shell/etc/profile.d/00-reset-path.sh
+++ base/conf/cuda/shell/etc/profile.d/00-reset-path.sh
@@ -1,7 +1,3 @@
 # Reset default PATH settings
-if [ "$(id -u)" -eq 0 ] || [ -n "$ZSH_VERSION" ]; then
-  PATH="/opt/code-server/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-else
-  PATH="/opt/code-server/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"
-fi
+PATH="/opt/code-server/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export PATH
