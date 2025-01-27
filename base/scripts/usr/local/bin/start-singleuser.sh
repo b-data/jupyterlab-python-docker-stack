4deab9affca4be2e92972fda6ea1dc9d6aba93db
Olivier Benz <olivier.benz@b-data.ch>
2025-01-27 13:30:28 +0100

    Do not override singleuser ip to 0.0.0.0
    - JupyterHub singleuser arguments are set using environment variables
    - https://github.com/jupyter/docker-stacks/pull/2203

--- base/scripts/usr/local/bin/start-singleuser.sh
+++ base/scripts/usr/local/bin/start-singleuser.sh
@@ -4,10 +4,7 @@
 
 set -e
 
-# set default ip to 0.0.0.0
-if [[ "${NOTEBOOK_ARGS} $*" != *"--ip="* ]]; then
-    NOTEBOOK_ARGS="--ip=0.0.0.0 ${NOTEBOOK_ARGS}"
-fi
+# JupyterHub singleuser arguments are set using environment variables
 
 echo "Executing: jupyterhub-singleuser" "${NOTEBOOK_ARGS}" "$@"
 # shellcheck disable=SC1091,SC2086
